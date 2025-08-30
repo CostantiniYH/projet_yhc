933a2d628d527865a167e8e465621d6af39cf28c
CostantiniYH <costantiniyaacov@gamil.com>
2025-08-31 00:47:53 +0200

    modif de l'index

--- categories.php
+++ categories.php
@@ -49,11 +49,11 @@ $navbar->render() ;
 
 <div class="container mb-5 mt-5">    
     <?php require_once __DIR__ . '/components/alerts.php'; ?>
-    
+
     <h1 class="mb-5 shadow rounded-4 border-start border-end border-2 border-success">Catégories</h1>    
     <div class="row gy-5 text-center">
         <?php foreach ($categories as $categorie) { ?>
-            <div class="col-md-4" data-aos="fade-up" data-aos-duration="2000">
+            <div class="col-md-4 col-sm-12" data-aos="fade-up" data-aos-duration="2000">
                 <?php require __DIR__ . '/components/categorie.php';?>            
             </div>
         <?php } ?>
