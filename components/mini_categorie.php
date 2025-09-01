b9620ea2ea7d5f1b7dca6161df926a9513779da4
CostantiniYH <costantiniyaacov@gamil.com>
2025-09-01 22:04:27 +0200

    dezd

--- components/mini_categorie.php
+++ components/mini_categorie.php
@@ -1,5 +1,5 @@
 <div class="rounded-4 shadow text-center">
-  <img src="<?= BASE_URL . $categorie['image']; ?>" class="card-img-top card-img rounded-4" 
+  <img src="<?=  $categorie['image']; ?>" class="card-img-top card-img rounded-4" 
   alt="..." style="height: 50px; overflow: hidden;" usemap="#map<?= $categorie['id']; ?>">
   <map name="map<?= $categorie['id']; ?>">
     <area shape="rect" coords="0,0,300,140" href="<?= BASE_URL ?>produits.php?id=<?= $categorie['id']; ?>">
