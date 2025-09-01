88ab17dfb68646bfe8503c61d55a66d090a85e33
CostantiniYH <costantiniyaacov@gamil.com>
2025-09-01 23:09:30 +0200

    dezd

---
+++ controllers/del_categorie.php
@@ -0,0 +1,32 @@
+<?php
+require_once __DIR__ . '/../backend/db_connect.php';
+require_once __DIR__ . '/../controllers/session.php';
+
+
+$id = $_GET['id'];
+
+if ($id) {
+    $pdo = connect();
+    $categorie = findBy1($pdo, 't_categories', 'id', $id);
+    $categorie = $categorie[0] ?? null;
+    $categorieNom = $categorie['nom'];
+    
+    if ($categorie) {
+        delete($pdo, 't_categories', $id, true);
+       // var_dump(delete($connect, 't_categories', $id, true));
+       // exit();
+        if (isAdmin()) {
+            header('Location: ' . BASE_URL . 'admin/dashboard_admin.php?success=' . urlencode("categorie $categorieNom supprimé avec succès !"));
+        } else {
+            header('Location: ' . BASE_URL . 'compte/dashboard.php?success=' . urlencode("Votre categorie $categorieNom a été supprimé avec succès !"));
+        }
+        exit();
+    } else {
+        header('Location: ' . BASE_URL . 'compte/dashboard.php?erreur=categorie introuvable.');
+        exit();
+    }
+} else {
+    header('Location: ' . BASE_URL . 'compte/dashboard.php?erreur=ID categorie manquant.');
+    exit();
+}
+?>
---
+++ controllers/del_image.php
@@ -0,0 +1,32 @@
+<?php
+require_once __DIR__ . '/../backend/db_connect.php';
+require_once __DIR__ . '/../controllers/session.php';
+
+
+$id = $_GET['id'];
+
+if ($id) {
+    $pdo = connect();
+    $image = findBy1($pdo, 't_images', 'id', $id);
+    $image = $image[0] ?? null;
+    $imageNom = $image['nom'];
+    
+    if ($image) {
+        delete($pdo, 't_images', $id, true);
+       // var_dump(delete($connect, 't_images', $id, true));
+       // exit();
+        if (isAdmin()) {
+            header('Location: ' . BASE_URL . 'admin/dashboard_admin.php?success=' . urlencode("image $imageNom supprimé avec succès !"));
+        } else {
+            header('Location: ' . BASE_URL . 'compte/dashboard.php?success=' . urlencode("Votre image $imageNom a été supprimé avec succès !"));
+        }
+        exit();
+    } else {
+        header('Location: ' . BASE_URL . 'compte/dashboard.php?erreur=image introuvable.');
+        exit();
+    }
+} else {
+    header('Location: ' . BASE_URL . 'compte/dashboard.php?erreur=ID image manquant.');
+    exit();
+}
+?>
