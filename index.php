4086b6e180bb8547551680bb232453930a54cb12
CostantiniYH <costantiniyaacov@gamil.com>
2025-09-04 21:51:55 +0200

    dhqksqjhd

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 ini_set('display_errors', 1);
 error_reporting(E_ALL);
 
+require_once __DIR__ . '/backend/db_connect.php';
 require_once __DIR__ . '/Controllers/session.php';
 require_once __DIR__ . '/components/header.php';
 require_once __DIR__ . '/class/navbar.php';
